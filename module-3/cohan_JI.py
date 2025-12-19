6c1d0a68b865715295710f6c8e4b7bf5cbb0aec0
Jorge.Ibarra <joibarra@my365.bellevue.edu>
2025-12-19 14:40:50 -0600

    Pushing flowchard and code

--- module-3/cohan_JI.py
+++ module-3/cohan_JI.py
@@ -17,6 +17,13 @@ dice total to an even (cho) or odd (han) number.
 If player rolls a 2 or a 7 they get 10 mon bonus!
 ''')
 
+#Jorge Ibarra
+#CSD325
+#Module 3.2 Assignment
+#12/19/2025
+
+#creating a function to check for sum of 2 or 7. Will pass in dice1, dice2, and purse from main loop
+#will print message and add 10 to purse if condition is met
 def isSum2Or7(dice1, dice2, purse):
     sum_dice = dice1 + dice2
     if sum_dice == 2 or sum_dice == 7:
@@ -24,11 +31,11 @@ def isSum2Or7(dice1, dice2, purse):
          print('You receive a 10 mon bonus!')
          purse = purse + 10
 
-purse = 20
+purse = 500
 while True:  # Main game loop.
     # Place your bet:
     #Added my initials with a colon to input prompt ji:
-    print('You have', purse, 'mon. How much do you bet? (or QUIT) ji: ')
+    print('You have', purse, 'mon. How much do you bet? (or QUIT) JI: ')
     while True:
         pot = input('> ')
         if pot.upper() == 'QUIT':
@@ -44,8 +51,8 @@ while True:  # Main game loop.
             break  # Exit the loop once a valid bet is placed.
 
     # Roll the dice.
-    dice1 = random.randint(1,1)
-    dice2 = random.randint(1,1)
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
 
     print('The dealer swirls the cup and you hear the rattle of dice.')
     print('The dealer slams the cup on the floor, still covering the')
@@ -55,6 +62,7 @@ while True:  # Main game loop.
 
     # Let the player bet cho or han:
     while True:
+
         bet = input('> ').upper()
         if bet != 'CHO' and bet != 'HAN':
             print('Please enter either "CHO" or "HAN".')
@@ -67,6 +75,7 @@ while True:  # Main game loop.
     print('  ', JAPANESE_NUMBERS[dice1], '-', JAPANESE_NUMBERS[dice2])
     print('    ', dice1, '-', dice2)
     
+    #calling the function to check for sum of 2 or 7
     isSum2Or7(dice1, dice2, purse)
 
     
@@ -81,16 +90,16 @@ while True:  # Main game loop.
     playerWon = bet == correctBet
 
     # Display the bet results:
+     # Display the bet results:
     if playerWon:
-         print('You won! You take', pot, 'mon.')
-         purse = purse + pot  # Add the pot to player's purse.
-
-         house_fee = pot * 12 // 100
-         print('The house collects a', house_fee, 'mon fee.')
-         purse = purse - house_fee
+        print('You won! You take', pot, 'mon.')
+        purse = purse + pot  # Add the pot from player's purse.
+        print('The house collects a', pot // 12, 'mon fee.')
+        purse = purse - (pot // 12)  # The house fee is 10%.
     else:
-             purse = purse - pot  # Subtract the pot from player's purse.
-             print('You lost!')
+        purse = purse - pot  # Subtract the pot from player's purse.
+        print('You lost!')
+        
     # Check if the player has run out of money:
     if purse == 0:
         print('You have run out of money!')
